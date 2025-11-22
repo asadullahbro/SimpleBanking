96fc12c9f216763119305bb425b9d6e0f761f365
Muhammad Asad Ullah <aliexpressdallas@gmail.com>
2025-11-23 04:09:20 +0500

    Update check_balance.py

--- check_balance.py
+++ check_balance.py
@@ -1,10 +1,6 @@
 from tkinter.font import names
-
 import customtkinter as ctk
 import json
-
-
-
 def check_balance():
     window = ctk.CTkToplevel()
     window.geometry("400x300")
@@ -31,3 +27,4 @@ def check_balance():
             name_entry.delete(0, "end")
     check_balance_btn = ctk.CTkButton(window, text="Check Balance", command=logic_check_balance)
     check_balance_btn.pack(pady=10)
+
